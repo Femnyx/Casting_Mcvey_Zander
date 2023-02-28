ecad478c436593a70d588b326b242e669f78a522
Zander <114107976+Femnyx@users.noreply.github.com>
2023-02-28 13:18:14 -0600

    The world indeed 1.05

--- Casting_Mcvey_Zander/Program.cs
+++ Casting_Mcvey_Zander/Program.cs
@@ -9,17 +9,17 @@ namespace Casting_Mcvey_Zander
             Console.WriteLine("Please enter a whole number, for the sake of the world...");
             int myFavNum = Convert.ToInt32(Console.ReadLine());
             
-            // Prints favorite number to console.
+            // Prints favorite number to console, FOR THE WORLD!
             Console.WriteLine("Your favorite number is " + myFavNum);
 
-            // Asks user a true/false question and assigns response to a boolean.
+            // Asks user a true/false question and assigns response to a boolean, asking if you want to save the world.
             Console.WriteLine("Would you like to save the world? (Please enter 'true' or 'false')");
             bool isTrue = Convert.ToBoolean(Console.ReadLine());
 
             //Print the result of question asked below. (Dew it)
             Console.WriteLine($"You said {isTrue}, I see.");
            
-            //The system starts to feel sinister, like it is up to something.
+            //The system starts to die off, your adventure has just begun it seems.
             Console.WriteLine("That is very, very...");
             Console.WriteLine("Interesting");
                 
